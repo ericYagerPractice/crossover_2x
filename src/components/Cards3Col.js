aa8555b1386b76bf6602db06062c8afc1dfa775c
Wesley Gibbs <wcgibbsey@gmail.com>
2020-12-31 10:02:57 -0500

    Slideshow and Card Updates

--- src/components/Cards3Col.js
+++ src/components/Cards3Col.js
@@ -9,6 +9,7 @@ import {
   MDBAnimation,
   MDBNavLink,
 } from 'mdbreact';
+import cardIcon1 from '../staticfiles/icons/cm-atomic-icon.png';
 
 export default function Cards3Col() {
     return(
@@ -16,7 +17,11 @@ export default function Cards3Col() {
           <MDBCol md='6' xl='4' className='px-2'>
             <MDBAnimation reveal type='fadeInLeft'>
               <MDBCard cascade className='my-2 px-2 py-2 border-light rounded-0 d-flex flex-row align-items-center'>
-                <MDBIcon icon="play-circle" className='mx-auto my-4 w-25 text-center' size="4x" />
+                <img
+                  className="d-block m-2 card-icon"
+                  src={cardIcon1}
+                  alt="Icon 1"
+                />
                 <MDBCardBody cascade className='w-75 py-0 pl-3 pr-0 text-left dark-text'>
                   <MDBCardText className='m-0 text-dark font-weight-bold text-uppercase card-category'>
                     Orange POD
@@ -42,38 +47,15 @@ export default function Cards3Col() {
 
           <MDBCol md='6' xl='4' className='px-2'>
             <MDBAnimation reveal type='fadeInLeft'>
-              <MDBCard cascade className='my-2 px-2 py-2 elegant-color-dark border-dark rounded-0 d-flex flex-row align-items-center'>
-                <MDBIcon fab icon="slack" className='mx-auto text-white my-4 w-30 text-center' size="4x" />
-                <MDBCardBody cascade className='w-75 py-0 pl-3 pr-0 text-white text-left'>
-                  <MDBCardText className='m-0 text-white font-weight-bold text-uppercase card-category'>
-                    Slack Onboard
-                  </MDBCardText>
-                  <MDBCardTitle className="mt-2 mb-1 text-white card-title--sm">
-                    Title
-                  </MDBCardTitle>
-                  <hr className="w-25 my-0 ml-0 mr-auto text-left border-light"/>
-                  <MDBCardText className='text-white mt-2'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </MDBCardText>
-                  <MDBNavLink
-                    tag='button'
-                    to='/CourseOverview'
-                    className='btn btn-outline-light btn-lg waves-effect w-75 px-2 py-2 mt-2 ml-0 d-flex align-items-center justify-content-center'
-                  >
-                    Get Started <MDBIcon icon='caret-right' className='ml-3' size='2x'></MDBIcon>
-                  </MDBNavLink>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBAnimation>
-          </MDBCol>
-
-          <MDBCol md='6' xl='4' className='px-2'>
-            <MDBAnimation reveal type='fadeInLeft'>
               <MDBCard cascade className='my-2 px-2 py-2 border-light rounded-0 d-flex flex-row align-items-center'>
-                <MDBIcon icon="file-download" className='mx-auto my-4 w-25 text-center' size="4x" />
+                <img
+                  className="d-block m-2 card-icon"
+                  src={cardIcon1}
+                  alt="Icon 1"
+                />
                 <MDBCardBody cascade className='w-75 py-0 pl-3 pr-0 text-left dark-text'>
                   <MDBCardText className='m-0 text-dark font-weight-bold text-uppercase card-category'>
-                    Case Study Primer
+                    Orange POD
                   </MDBCardText>
                   <MDBCardTitle className="mt-2 mb-1 card-title--sm">
                     Title
@@ -85,9 +67,9 @@ export default function Cards3Col() {
                   <MDBNavLink
                     tag='button'
                     to='/CourseOverview'
-                    className='btn btn-outline-dark btn-lg w-75 px-2 py-2 mt-2 ml-0 shadow-none d-flex align-items-center justify-content-center'
+                    className='btn btn-danger btn-lg w-75 px-2 py-2 mt-2 ml-0 shadow-none d-flex align-items-center justify-content-center'
                   >
-                    Get Info <MDBIcon icon='caret-right' className='ml-3' size='2x'></MDBIcon>
+                    Start POD <MDBIcon icon='caret-right' className='ml-3' size='2x'></MDBIcon>
                   </MDBNavLink>
                 </MDBCardBody>
               </MDBCard>
@@ -96,79 +78,29 @@ export default function Cards3Col() {
 
           <MDBCol md='6' xl='4' className='px-2'>
             <MDBAnimation reveal type='fadeInLeft'>
-              <MDBCard cascade className='my-2 px-2 py-2 rounded-0 border-dark d-flex flex-row align-items-center'>
-                <MDBIcon icon="video" className='mx-auto my-4 w-25 text-center' size="4x" />
+              <MDBCard cascade className='my-2 px-2 py-2 border-light rounded-0 d-flex flex-row align-items-center'>
+                <img
+                  className="d-block m-2 card-icon"
+                  src={cardIcon1}
+                  alt="Icon 1"
+                />
                 <MDBCardBody cascade className='w-75 py-0 pl-3 pr-0 text-left dark-text'>
                   <MDBCardText className='m-0 text-dark font-weight-bold text-uppercase card-category'>
-                    Video Primer
+                    Orange POD
                   </MDBCardText>
                   <MDBCardTitle className="mt-2 mb-1 card-title--sm">
                     Title
                   </MDBCardTitle>
-                  <hr className="w-25 my-0 ml-0 mr-auto text-left border-dark"/>
+                  <hr className="w-25 my-0 ml-0 mr-auto text-left border-danger"/>
                   <MDBCardText className='text-dark mt-2'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   </MDBCardText>
                   <MDBNavLink
                     tag='button'
                     to='/CourseOverview'
-                    className='btn btn-outline-dark btn-lg shadow-none w-75 px-2 py-2 mt-2 ml-0 d-flex align-items-center justify-content-center'
+                    className='btn btn-danger btn-lg w-75 px-2 py-2 mt-2 ml-0 shadow-none d-flex align-items-center justify-content-center'
                   >
-                    Watch Now <MDBIcon icon='caret-right' className='ml-3' size='2x'></MDBIcon>
-                  </MDBNavLink>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBAnimation>
-          </MDBCol>
-
-          <MDBCol md='6' xl='4' className='px-2'>
-            <MDBAnimation reveal type='fadeInLeft'>
-              <MDBCard cascade className='my-2 px-2 py-2 elegant-color-dark border-dark rounded-0 d-flex flex-row align-items-center'>
-                <MDBIcon icon="cogs" className='mx-auto text-white my-4 w-30 text-center' size="4x" />
-                <MDBCardBody cascade className='w-75 py-0 pl-3 pr-0 text-white text-left'>
-                  <MDBCardText className='m-0 text-white font-weight-bold text-uppercase card-category'>
-                    Idea Session
-                  </MDBCardText>
-                  <MDBCardTitle className="mt-2 mb-1 text-white card-title--sm">
-                    Title
-                  </MDBCardTitle>
-                  <hr className="w-25 my-0 ml-0 mr-auto text-left border-light"/>
-                  <MDBCardText className='text-white mt-2'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </MDBCardText>
-                  <MDBNavLink
-                    tag='button'
-                    to='/CourseOverview'
-                    className='btn btn-outline-light btn-lg waves-effect w-75 px-2 py-2 mt-2 ml-0 d-flex align-items-center justify-content-center'
-                  >
-                    Get Started <MDBIcon icon='caret-right' className='ml-3' size='2x'></MDBIcon>
-                  </MDBNavLink>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBAnimation>
-          </MDBCol>
-
-          <MDBCol md='6' xl='4' className='px-2'>
-            <MDBAnimation reveal type='fadeInLeft'>
-              <MDBCard cascade className='my-2 px-2 py-2 rounded-0 border-dark d-flex flex-row align-items-center'>
-                <MDBIcon icon="user-plus" className='mx-auto my-4 w-25 text-center' size="4x" />
-                <MDBCardBody cascade className='w-75 py-0 pl-3 pr-0 text-left dark-text'>
-                  <MDBCardText className='m-0 text-dark font-weight-bold text-uppercase card-category'>
-                    Create Account
-                  </MDBCardText>
-                  <MDBCardTitle className="mt-2 mb-1 card-title--sm">
-                    Title
-                  </MDBCardTitle>
-                  <hr className="w-25 my-0 ml-0 mr-auto text-left border-dark"/>
-                  <MDBCardText className='text-dark mt-2'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </MDBCardText>
-                  <MDBNavLink
-                    tag='button'
-                    to='/CourseOverview'
-                    className='btn btn-outline-danger btn-lg shadow-none w-75 px-2 py-2 mt-2 ml-0 d-flex align-items-center justify-content-center'
-                  >
-                    Sign Up <MDBIcon icon='caret-right' className='ml-3' size='2x'></MDBIcon>
+                    Start POD <MDBIcon icon='caret-right' className='ml-3' size='2x'></MDBIcon>
                   </MDBNavLink>
                 </MDBCardBody>
               </MDBCard>
--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -6,30 +6,39 @@ import {
     MDBCarouselItem,
     MDBView
 } from 'mdbreact';
-import blocSlide1 from '../staticfiles/carousel-slides/cpts-4x-bloc-infograpic-1.jpg';
-import blocSlide2 from '../staticfiles/carousel-slides/cpts-4x-bloc-infograpic-2.jpg';
-import blocSlide3 from '../staticfiles/carousel-slides/cpts-4x-bloc-infograpic-3.jpg';
+import slide1 from '../staticfiles/carousel-slides/c2x-intro-slide-1.jpg';
+import slide2 from '../staticfiles/carousel-slides/c2x-intro-slide-2.jpg';
+import slide3 from '../staticfiles/carousel-slides/c2x-intro-slide-3.jpg';
+import slide4 from '../staticfiles/carousel-slides/c2x-intro-slide-4.jpg';
+import slide5 from '../staticfiles/carousel-slides/c2x-intro-slide-5.jpg';
+import slide6 from '../staticfiles/carousel-slides/c2x-intro-slide-6.jpg';
+import slide7 from '../staticfiles/carousel-slides/c2x-intro-slide-7.jpg';
+import slide8 from '../staticfiles/carousel-slides/c2x-intro-slide-8.jpg';
+import slide9 from '../staticfiles/carousel-slides/c2x-intro-slide-9.jpg';
+import slide10 from '../staticfiles/carousel-slides/c2x-intro-slide-10.jpg';
+import slide11 from '../staticfiles/carousel-slides/c2x-intro-slide-11.jpg';
+import slide12 from '../staticfiles/carousel-slides/c2x-intro-slide-12.jpg';
 
 export default function ImageCarousel() {
     return(
         <>
           <MDBContainer className="p-0">
             <MDBCarousel
-              className="pb-5"
+              className="pb-5 carousel--primary"
               activeItem={1}
-              length={3}
+              length={12}
               showControls={false}
               showIndicators={true}
-              slide={true}
-              interval={2200}
+              fade={true}
+              interval={2600}
             >
               <MDBCarouselInner>
                 <MDBCarouselItem itemId="1">
                   <MDBView>
                     <img
                       className="d-block w-100"
-                      src={blocSlide1}
-                      alt="Crossover 2X Bloc Slide 1"
+                      src={slide1}
+                      alt="Crossover 2X Intro Slide 1"
                     />
                   </MDBView>
                 </MDBCarouselItem>
@@ -37,8 +46,8 @@ export default function ImageCarousel() {
                   <MDBView>
                     <img
                       className="d-block w-100"
-                      src={blocSlide2}
-                      alt="Crossover 2X Bloc Slide 2"
+                      src={slide2}
+                      alt="Crossover 2X Intro Slide 2"
                     />
                   </MDBView>
                 </MDBCarouselItem>
@@ -46,8 +55,89 @@ export default function ImageCarousel() {
                   <MDBView>
                     <img
                       className="d-block w-100"
-                      src={blocSlide3}
-                      alt="Crossover 2X Bloc Slide 3"
+                      src={slide3}
+                      alt="Crossover 2X Intro Slide 3"
+                    />
+                  </MDBView>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="4">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={slide4}
+                      alt="Crossover 2X Intro Slide 4"
+                    />
+                  </MDBView>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="5">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={slide5}
+                      alt="Crossover 2X Intro Slide 5"
+                    />
+                  </MDBView>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="6">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={slide6}
+                      alt="Crossover 2X Intro Slide 6"
+                    />
+                  </MDBView>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="7">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={slide7}
+                      alt="Crossover 2X Intro Slide 7"
+                    />
+                  </MDBView>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="8">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={slide8}
+                      alt="Crossover 2X Intro Slide 8"
+                    />
+                  </MDBView>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="9">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={slide9}
+                      alt="Crossover 2X Intro Slide 9"
+                    />
+                  </MDBView>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="10">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={slide10}
+                      alt="Crossover 2X Intro Slide 10"
+                    />
+                  </MDBView>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="11">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={slide11}
+                      alt="Crossover 2X Intro Slide 11"
+                    />
+                  </MDBView>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="12">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={slide12}
+                      alt="Crossover 2X Intro Slide 12"
                     />
                   </MDBView>
                 </MDBCarouselItem>
