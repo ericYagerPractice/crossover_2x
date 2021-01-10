3413cecf245ef6664d84a5a6131c995d59bf0f98
Wesley Gibbs <wcgibbsey@gmail.com>
2021-01-09 20:27:53 -0500

    Card Updates

--- src/components/Cards3Col.js
+++ src/components/Cards3Col.js
@@ -32,13 +32,11 @@ export default function Cards3Col() {
                     Challenge Pod
                   </MDBCardTitle>
                   <hr className="w-25 my-0 ml-0 mr-auto text-left border-danger"/>
-                  <MDBCardText className='text-dark mt-2'>
-                    <ul>
-                      <li><a href="#" className="color-dark">Habit Challenge</a></li>
-                      <li><a href="#">Sleep Challenge</a></li>
-                      <li><a href="#">Diet Challenge</a></li>
-                    </ul>
-                  </MDBCardText>
+                  <ul className='text-dark mt-2 card-list'>
+                    <li><a href="#" className="color-dark">Burnout</a></li>
+                    <li><a href="#">Self-Awareness</a></li>
+                    <li><a href="#">AI & Machine Learning</a></li>
+                  </ul>
                   <MDBNavLink
                     tag='button'
                     to='/CourseOverview'
@@ -67,13 +65,11 @@ export default function Cards3Col() {
                     Rise & Fall of
                   </MDBCardTitle>
                   <hr className="w-25 my-0 ml-0 mr-auto text-left border-danger"/>
-                  <MDBCardText className='text-dark mt-2'>
-                    <ul>
-                      <li><a href="#" className="color-dark">WeWork</a></li>
-                      <li><a href="#">Uber</a></li>
-                      <li><a href="#">Theranos</a></li>
-                    </ul>
-                  </MDBCardText>
+                  <ul className='text-dark mt-2 card-list'>
+                    <li><a href="#" className="color-dark">Burnout</a></li>
+                    <li><a href="#">Self-Awareness</a></li>
+                    <li><a href="#">AI & Machine Learning</a></li>
+                  </ul>
                   <MDBNavLink
                     tag='button'
                     to='/CourseOverview'
@@ -102,13 +98,110 @@ export default function Cards3Col() {
                     Guide to X
                   </MDBCardTitle>
                   <hr className="w-25 my-0 ml-0 mr-auto text-left border-danger"/>
-                  <MDBCardText className='text-dark mt-2'>
-                    <ul>
-                      <li><a href="#" className="color-dark">Burnout</a></li>
-                      <li><a href="#">Self-Awareness</a></li>
-                      <li><a href="#">AI & Machine Learning</a></li>
-                    </ul>
+                  <ul className='text-dark mt-2 card-list'>
+                    <li><a href="#" className="color-dark">Burnout</a></li>
+                    <li><a href="#">Self-Awareness</a></li>
+                    <li><a href="#">AI & Machine Learning</a></li>
+                  </ul>
+                  <MDBNavLink
+                    tag='button'
+                    to='/CourseOverview'
+                    className='btn btn-lg btn-outline-dark w-75 px-2 py-1 mt-2 ml-0 shadow-none d-flex align-items-center justify-content-center'
+                  >
+                    Buy In <MDBIcon icon='caret-right' className='ml-3' size='2x'></MDBIcon>
+                  </MDBNavLink>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBAnimation>
+          </MDBCol>
+
+          <MDBCol md='6' xl='4' className='px-2'>
+            <MDBAnimation reveal type='fadeInLeft'>
+              <MDBCard cascade className='my-2 px-2 py-2 border-dark rounded-0 d-flex flex-row align-items-center'>
+                <img
+                  className="d-block m-2 card-icon"
+                  src={cardIcon1}
+                  alt="Icon 1"
+                />
+                <MDBCardBody cascade className='w-75 py-0 pl-3 pr-0 text-left dark-text'>
+                  <MDBCardText className='m-0 text-dark font-weight-bold text-uppercase card-category'>
+                    Channel
                   </MDBCardText>
+                  <MDBCardTitle className="my-1 card-title--sm">
+                    Challenge Pod
+                  </MDBCardTitle>
+                  <hr className="w-25 my-0 ml-0 mr-auto text-left border-danger"/>
+                  <ul className='text-dark mt-2 card-list'>
+                    <li><a href="#" className="color-dark">Burnout</a></li>
+                    <li><a href="#">Self-Awareness</a></li>
+                    <li><a href="#">AI & Machine Learning</a></li>
+                  </ul>
+                  <MDBNavLink
+                    tag='button'
+                    to='/CourseOverview'
+                    className='btn btn-lg btn-outline-dark w-75 px-2 py-1 mt-2 ml-0 shadow-none d-flex align-items-center justify-content-center'
+                  >
+                    Buy In <MDBIcon icon='caret-right' className='ml-3' size='2x'></MDBIcon>
+                  </MDBNavLink>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBAnimation>
+          </MDBCol>
+
+          <MDBCol md='6' xl='4' className='px-2'>
+            <MDBAnimation reveal type='fadeInLeft'>
+              <MDBCard cascade className='my-2 px-2 py-2 border-dark rounded-0 d-flex flex-row align-items-center'>
+                <img
+                  className="d-block m-2 card-icon"
+                  src={cardIcon2}
+                  alt="Icon 1"
+                />
+                <MDBCardBody cascade className='w-75 py-0 pl-3 pr-0 text-left dark-text'>
+                  <MDBCardText className='m-0 text-dark font-weight-bold text-uppercase card-category'>
+                    Case Menu
+                  </MDBCardText>
+                  <MDBCardTitle className="my-1 card-title--sm">
+                    Rise & Fall of
+                  </MDBCardTitle>
+                  <hr className="w-25 my-0 ml-0 mr-auto text-left border-danger"/>
+                  <ul className='text-dark mt-2 card-list'>
+                    <li><a href="#" className="color-dark">Burnout</a></li>
+                    <li><a href="#">Self-Awareness</a></li>
+                    <li><a href="#">AI & Machine Learning</a></li>
+                  </ul>
+                  <MDBNavLink
+                    tag='button'
+                    to='/CourseOverview'
+                    className='btn btn-lg btn-outline-dark w-75 px-2 py-1 mt-2 ml-0 shadow-none d-flex align-items-center justify-content-center'
+                  >
+                    Buy In <MDBIcon icon='caret-right' className='ml-3' size='2x'></MDBIcon>
+                  </MDBNavLink>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBAnimation>
+          </MDBCol>
+
+          <MDBCol md='6' xl='4' className='px-2'>
+            <MDBAnimation reveal type='fadeInLeft'>
+              <MDBCard cascade className='my-2 px-2 py-2 border-dark rounded-0 d-flex flex-row align-items-center'>
+                <img
+                  className="d-block m-2 card-icon"
+                  src={cardIcon3}
+                  alt="Icon 1"
+                />
+                <MDBCardBody cascade className='w-75 py-0 pl-3 pr-0 text-left dark-text'>
+                  <MDBCardText className='m-0 text-dark font-weight-bold text-uppercase card-category'>
+                    Primer
+                  </MDBCardText>
+                  <MDBCardTitle className="my-1 card-title--sm">
+                    Guide to X
+                  </MDBCardTitle>
+                  <hr className="w-25 my-0 ml-0 mr-auto text-left border-danger"/>
+                  <ul className='text-dark mt-2 card-list'>
+                    <li><a href="#" className="color-dark">Burnout</a></li>
+                    <li><a href="#">Self-Awareness</a></li>
+                    <li><a href="#">AI & Machine Learning</a></li>
+                  </ul>
                   <MDBNavLink
                     tag='button'
                     to='/CourseOverview'
