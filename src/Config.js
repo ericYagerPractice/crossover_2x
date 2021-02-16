1e977f9e9ea7f900dfc2f37bed51ff1ad0cebf38
Eric Yager <eyager@amazon.com>
2021-02-16 12:10:23 -0600

    config updates

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 const RootPathways = {
     "localhost": "http://localhost:3000",
-    "devBuild": "https://main.d3s7jqy33emiq1.amplifyapp.com/",
+    "devBuild": "https://feature.d3s7jqy33emiq1.amplifyapp.com/",
     "prodBuild": "https://crossover2x.net",
 };
 
