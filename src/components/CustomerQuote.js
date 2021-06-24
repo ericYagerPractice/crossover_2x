f5268acc8af8971af151af3423c4ac88f728acd2
Wesley Gibbs <wcgibbsey@gmail.com>
2021-06-24 09:56:00 -0400

    Quote updates

--- src/components/CustomerQuote.js
+++ src/components/CustomerQuote.js
@@ -24,8 +24,8 @@ class CustomerQuote extends React.Component {
       <MDBContainer className="customer-quote customer-quote--dark mt-5 w-100 mw-100 p-4 p-lg-5 sprint-overview-quote">
         <MDBRow className="p-0">
           <MDBCol className="p-0 text-center text-white d-flex align-items-center justify-content-center flex-column">
-            <h2>"C2X was an amazing learning experience and really helped me level up my skill set."</h2>
-            <p>- Quote Name, Recent MBA Grad.</p>
+            <h2><MDBIcon icon="quote-left mr-2"/> C2X was an amazing learning experience and really helped me level up my skill set. <MDBIcon icon="quote-right ml-2" fontSize="small" /></h2>
+            <p> - Jim Davis, VP of Sales.</p>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
